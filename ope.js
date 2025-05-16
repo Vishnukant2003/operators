8376de73b1d56acd9a31c5b844e78ede840dfdab
Vishnukant Dnyanoba Gadekar <157220206+Vishnukant2003@users.noreply.github.com>
2025-05-16 16:29:16 +0530

    Add files via upload

---
+++ ope.js
@@ -0,0 +1,14 @@
+let num1 = 20;
+let num2 = 10;
+///addition 
+console.log(num1 + num2);
+//subtraction
+console.log(num1 - num2);
+//multiplication
+console.log(num1 * num2);
+//division
+console.log(num1 / num2);
+//modulus
+console.log(num1 % num2);
+//assign
+console.log(num1 = num2);
